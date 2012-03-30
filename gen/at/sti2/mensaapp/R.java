8cfab0f299cd19370fcaf35671c18e9d445d96b0
Michael Dejori <michael@dejori.eu>
2012-03-30 17:17:23 +0200

    deleted error files and sprit things

--- gen/at/sti2/mensaapp/R.java
+++ gen/at/sti2/mensaapp/R.java
@@ -8,16 +8,26 @@
 package at.sti2.mensaapp;
 
 public final class R {
+    public static final class array {
+        public static final int static_cities=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int button1=0x7f060001;
+        public static final int citySpinner=0x7f060000;
+        public static final int mapview=0x7f060002;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int map=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int mensen_in=0x7f040002;
     }
 }
--- src/at/sti2/mensaapp/MensaAppAndroidActivity.java
+++ src/at/sti2/mensaapp/MensaAppAndroidActivity.java
@@ -2,12 +2,21 @@ package at.sti2.mensaapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import at.sti2.mensaapp.R;
+
 
 public class MensaAppAndroidActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	@Override
+	protected void onCreate(Bundle icicle) {
+		// TODO Auto-generated method stub
+		super.onCreate(icicle);
+		setContentView(R.layout.main);
+
+	}
+
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
