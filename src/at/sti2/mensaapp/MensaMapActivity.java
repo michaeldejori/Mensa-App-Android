67dada0c3bde2d5857a15d6c51bb8aae4294a9b5
Michael Dejori <michael@dejori.eu>
2012-03-30 17:19:14 +0200

    deleted error files and sprit things

---
+++ src/at/sti2/mensaapp/MensaMapActivity.java
@@ -0,0 +1,39 @@
+package at.sti2.mensaapp;
+
+import android.os.Bundle;
+import at.sti2.mensaapp.R;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+
+public class MensaMapActivity extends MapActivity {
+
+	private MapView mapView;
+
+	@Override
+	protected void onCreate(Bundle icicle) {
+		// TODO Auto-generated method stub
+		super.onCreate(icicle);
+		setContentView(R.layout.map);
+
+		mapView = (MapView) findViewById(R.id.mapview);
+		mapView.setBuiltInZoomControls(true);
+
+		GeoPoint ibk = new GeoPoint((int) (47.267222 * 1E6),
+				(int) (11.392778 * 1E6));
+
+		MapController mapController = mapView.getController();
+		mapController.setCenter(ibk);
+		mapController.setZoom(12);
+
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
